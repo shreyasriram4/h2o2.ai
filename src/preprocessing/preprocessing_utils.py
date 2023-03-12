3a0bd59ac052e9a6e338cb0e5e6bd7a8a8379c32
cliftonfelix <cliftonfelix12@yahoo.com>
2023-03-12 13:28:22 +0800

    Reorder functions in preprocessing_utils.py

--- src/preprocessing/preprocessing_utils.py
+++ src/preprocessing/preprocessing_utils.py
@@ -12,16 +12,8 @@ def convert_sentiment_df(df: pd.DataFrame) -> pd.DataFrame:
         )
     return df
 
-def strip_html_tags_df(df: pd.DataFrame, src_col: str = "cleaned_text", dst_col: str = "cleaned_text") -> pd.DataFrame:
-    df[dst_col] = df[src_col].apply(
-        lambda text: re.sub('<[^<]+?>', ' ', text)
-        )
-    return df
-
-def lowercase_string_df(df: pd.DataFrame) -> pd.DataFrame:
-    df["cleaned_text"] = df["cleaned_text"].apply(
-        lambda text: text.lower()
-        )
+def expand_contractions_df(df: pd.DataFrame) -> pd.DataFrame:    
+    df["cleaned_text"] = df["Text"].apply(expand_contractions_text)
     return df
 
 def expand_contractions_text(text: str) -> str:
@@ -31,23 +23,10 @@ def expand_contractions_text(text: str) -> str:
         
     return ' '.join(expanded_words)
 
-def expand_contractions_df(df: pd.DataFrame) -> pd.DataFrame:    
-    df["cleaned_text"] = df["Text"].apply(expand_contractions_text)
-    return df
-
-def remove_punctuations_df(df: pd.DataFrame) -> pd.DataFrame:
+def lowercase_string_df(df: pd.DataFrame) -> pd.DataFrame:
     df["cleaned_text"] = df["cleaned_text"].apply(
-        lambda text: text.translate(str.maketrans('', '', string.punctuation))
+        lambda text: text.lower()
         )
-    return df
-
-def remove_stopwords_text(text: str) -> str:
-    return " ".join([w for w in text.split(" ") if not w in STOP_WORDS])
-    
-def remove_stopwords_df(df: pd.DataFrame) -> pd.DataFrame:
-    df["cleaned_text"] = df["cleaned_text"].apply(
-        remove_stopwords_text
-    )
     return df
 
 def remove_numbers_df(df: pd.DataFrame) -> pd.DataFrame:
@@ -56,8 +35,29 @@ def remove_numbers_df(df: pd.DataFrame) -> pd.DataFrame:
         )
     return df
 
+def remove_punctuations_df(df: pd.DataFrame) -> pd.DataFrame:
+    df["cleaned_text"] = df["cleaned_text"].apply(
+        lambda text: text.translate(str.maketrans('', '', string.punctuation))
+        )
+    return df
+
+def remove_stopwords_df(df: pd.DataFrame) -> pd.DataFrame:
+    df["cleaned_text"] = df["cleaned_text"].apply(
+        remove_stopwords_text
+    )
+    return df
+
+def remove_stopwords_text(text: str) -> str:
+    return " ".join([w for w in text.split(" ") if not w in STOP_WORDS])
+
 def replace_multiple_spaces_df(df: pd.DataFrame, src_col: str = "cleaned_text", dst_col: str = "cleaned_text") -> pd.DataFrame:
     df[dst_col] = df[src_col].apply(
         lambda text: re.sub(' +', ' ', text)
     )
+    return df
+
+def strip_html_tags_df(df: pd.DataFrame, src_col: str = "cleaned_text", dst_col: str = "cleaned_text") -> pd.DataFrame:
+    df[dst_col] = df[src_col].apply(
+        lambda text: re.sub('<[^<]+?>', ' ', text)
+        )
     return df
