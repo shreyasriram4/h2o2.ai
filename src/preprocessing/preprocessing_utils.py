b2a9fcafcece039e230eaa0ba78da72a2447bfef
cliftonfelix <cliftonfelix12@yahoo.com>
2023-03-12 13:43:25 +0800

    Edit function parameters in preprocessing_utils.py

--- src/preprocessing/preprocessing_utils.py
+++ src/preprocessing/preprocessing_utils.py
@@ -6,14 +6,14 @@ from nltk.corpus import stopwords
 
 STOP_WORDS = set(stopwords.words('english'))
 
-def convert_sentiment_df(df: pd.DataFrame) -> pd.DataFrame:
-    df["sentiment"] = df["Sentiment"].apply(
+def convert_sentiment_df(df: pd.DataFrame, src_col: str = "Sentiment", dst_col: str = "sentiment") -> pd.DataFrame:
+    df[dst_col] = df[src_col].apply(
         lambda x: 1 if x == "positive" else 0
         )
     return df
 
-def expand_contractions_df(df: pd.DataFrame) -> pd.DataFrame:    
-    df["cleaned_text"] = df["Text"].apply(expand_contractions_text)
+def expand_contractions_df(df: pd.DataFrame, src_col: str = "cleaned_text", dst_col: str = "cleaned_text") -> pd.DataFrame:    
+    df[dst_col] = df[src_col].apply(expand_contractions_text)
     return df
 
 def expand_contractions_text(text: str) -> str:
@@ -23,26 +23,26 @@ def expand_contractions_text(text: str) -> str:
         
     return ' '.join(expanded_words)
 
-def lowercase_string_df(df: pd.DataFrame) -> pd.DataFrame:
-    df["cleaned_text"] = df["cleaned_text"].apply(
+def lowercase_string_df(df: pd.DataFrame, src_col: str = "cleaned_text", dst_col: str = "cleaned_text") -> pd.DataFrame:
+    df[dst_col] = df[src_col].apply(
         lambda text: text.lower()
         )
     return df
 
-def remove_numbers_df(df: pd.DataFrame) -> pd.DataFrame:
-    df["cleaned_text"] = df["cleaned_text"].apply(
+def remove_numbers_df(df: pd.DataFrame, src_col: str = "cleaned_text", dst_col: str = "cleaned_text") -> pd.DataFrame:
+    df[dst_col] = df[src_col].apply(
         lambda text: ''.join([i for i in text if not i.isdigit()])
         )
     return df
 
-def remove_punctuations_df(df: pd.DataFrame) -> pd.DataFrame:
-    df["cleaned_text"] = df["cleaned_text"].apply(
+def remove_punctuations_df(df: pd.DataFrame, src_col: str = "cleaned_text", dst_col: str = "cleaned_text") -> pd.DataFrame:
+    df[dst_col] = df[src_col].apply(
         lambda text: text.translate(str.maketrans('', '', string.punctuation))
         )
     return df
 
-def remove_stopwords_df(df: pd.DataFrame) -> pd.DataFrame:
-    df["cleaned_text"] = df["cleaned_text"].apply(
+def remove_stopwords_df(df: pd.DataFrame, src_col: str = "cleaned_text", dst_col: str = "cleaned_text") -> pd.DataFrame:
+    df[dst_col] = df[src_col].apply(
         remove_stopwords_text
     )
     return df
