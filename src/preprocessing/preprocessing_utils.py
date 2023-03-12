f6825d14b3429d771dff08b0699de426fd317bac
cliftonfelix <cliftonfelix12@yahoo.com>
2023-03-12 13:16:20 +0800

    Add strip_html_tags_df for preprocessing

--- src/preprocessing/preprocessing_utils.py
+++ src/preprocessing/preprocessing_utils.py
@@ -1,7 +1,14 @@
 import pandas as pd
+import re
 
 def convert_sentiment_df(df: pd.DataFrame) -> pd.DataFrame:
     df["sentiment"] = df["Sentiment"].apply(
         lambda x: 1 if x == "positive" else 0
+        )
+    return df
+
+def strip_html_tags_df(df: pd.DataFrame, src_col: str = "cleaned_text", dst_col: str = "cleaned_text") -> pd.DataFrame:
+    df[dst_col] = df[src_col].apply(
+        lambda text: re.sub('<[^<]+?>', ' ', text)
         )
     return df
