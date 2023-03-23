60a7457cc883abc6f2c3ea93ec643eaca4a578df
cliftonfelix <cliftonfelix12@yahoo.com>
2023-03-23 14:14:08 +0800

    Add Lbl2Vec to predict_topic function

--- src/models/topic_modelling/test/predict.py
+++ src/models/topic_modelling/test/predict.py
@@ -1,27 +1,18 @@
 import pandas as pd
 from src.utils.file_util import FileUtil
+from src.models.topic_modelling.test.lbl2vec import Lbl2Vec
 from src.models.topic_modelling.test.zero_shot import ZeroShot
 
-def dataloader(df):
-    for i in range(len(df)):
-        yield df.loc[i, "cleaned_text"]
+def predict_topic(df, model_name = "Lbl2Vec"):
+    assert model_name in ["ZeroShot", "Lbl2Vec"]
 
-def predict_topic(df):
     topic_dict = FileUtil.get_topics()
 
-    candidate_labels = topic_dict["topics"]
-    clf = ZeroShot().get_model()
-
-    hypothesis_template = "The topic of this review is {}."
-
-    single_topic_prediction = clf(
-        dataloader(df),
-        candidate_labels, 
-        hypothesis_template = hypothesis_template
-        )
-
-    single_topic_prediction = pd.DataFrame(single_topic_prediction)
-
-    df['topic'] = single_topic_prediction['labels'].apply(lambda x: x[0])
+    if model_name == "ZeroShot":
+        candidate_labels = topic_dict["topics"]
+        df = ZeroShot().predict(df, "cleaned_text", candidate_labels)
+    else:
+        candidate_labels = topic_dict["topic_mapping"]
+        df = Lbl2Vec().predict(df, "cleaned_text", candidate_labels)
 
     return df
