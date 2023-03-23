5df319891777952871270d028a595dd18e159779
cliftonfelix <cliftonfelix12@yahoo.com>
2023-03-23 14:14:31 +0800

    Refactor zero shot

--- src/models/topic_modelling/test/zero_shot.py
+++ src/models/topic_modelling/test/zero_shot.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from src.utils.file_util import FileUtil
 from transformers import pipeline
 
@@ -14,6 +15,25 @@ class ZeroShot():
         if not FileUtil.check_filepath_exists(model_file_path):
             self.save_model()
         return FileUtil.get_topic_model()
+    
+    def dataloader(self, df, column):
+        for i in range(len(df)):
+            yield df.loc[i, column]
+    
+    def predict(self, df, column, candidate_labels):
+        clf = self.get_model()
+        hypothesis_template = "The topic of this review is {}."
+
+        preds = clf(
+            self.dataloader(df, column),
+            candidate_labels, 
+            hypothesis_template = hypothesis_template
+            )
+
+        preds = pd.DataFrame(preds)
+        df['topic'] = preds['labels'].apply(lambda x: x[0])
+
+        return df
     
 if __name__ == "__main__":
     ZeroShot().save_model()
