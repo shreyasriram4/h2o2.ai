ed8bba97ee40c3425a1c344983cdc4f816e04eb8
cliftonfelix <cliftonfelix12@yahoo.com>
2023-03-24 02:46:34 +0800

    Add sentiment prediction

--- src/models/predict.py
+++ src/models/predict.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from datetime import datetime
 from src.utils.file_util import FileUtil
-# from src.models.sentiment_analysis.test.predict import predict_sentiment
+from src.models.sentiment_analysis.test.predict import predict_sentiment
 from src.models.topic_modelling.test.predict import predict_topic
 from src.preprocessing.transformations import apply_cleaning
 
@@ -13,7 +13,7 @@ def predict_sentiment_topic(file_path, df = FileUtil.get_raw_train_data()):
 
     df = df.iloc[:10,] #to be removed
 
-    # df = predict_sentiment(df)
+    df = predict_sentiment(df)
     df = predict_topic(df)
 
     dt_string = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
--- src/models/sentiment_analysis/test/predict.py
+++ src/models/sentiment_analysis/test/predict.py
@@ -1,37 +1,11 @@
-# pending until BERT training is done
+from src.models.sentiment_analysis.train.bert import BERT
 
+def predict_sentiment(df):
+    model = BERT(True)
 
+    label, probs, tf_predictions = model.predict(df)
 
-# import os
-# import pandas as pd
-# import yaml
-# from src.models.topic_modelling.test.zero_shot import ZeroShot
+    df["sentiment"] = label
+    df["sentiment_prob"] = probs
 
-# PROJECT_DIR = os.path.abspath(
-#     os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
-# )
-# SENTIMENT_ANALYSIS_DIR = os.path.join(PROJECT_DIR, "src/models/sentiment_analysis")
-# SENTIMENT_ANALYSIS_MODEL_FILE_NAME = "model.pkl"
-
-# def predict_topic(df):
-#     with open(os.path.join(SENTIMENT_ANALYSIS_DIR, SENTIMENT_ANALYSIS_MODEL_FILE_NAME), 'r') as file:
-#         clf = pickle.load(f)
-
-#     clf = ZeroShot().get_model()
-
-#     hypothesis_template = "The topic of this review is {}."
-
-#     single_topic_prediction = clf(
-#         list(df["cleaned_text"]), 
-#         candidate_labels, 
-#         hypothesis_template = hypothesis_template
-#         )
-
-#     single_topic_prediction = pd.DataFrame(single_topic_prediction)
-
-#     df['topic'] = single_topic_prediction['labels'].apply(lambda x: x[0])
-
-#     return df
-
-# if __name__ == "__main__":
-#     predict_topic()
+    return df
