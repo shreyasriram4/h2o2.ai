3bd5a0fc82441df1b2b0c918cf0f13d66c69a4bb
cliftonfelix <cliftonfelix12@yahoo.com>
2023-03-25 23:21:10 +0800

    Implement pycodestyle in app.topics.py

--- src/app/topics.py
+++ src/app/topics.py
@@ -1,17 +1,17 @@
 from h2o_wave import main, app, Q, ui, on, handle_on, data
 from helper import add_card, clear_cards
-    
+
 
 @on('#topics')
 async def page3(q: Q):
     q.page['sidebar'].value = '#topics'
-    clear_cards(q)  # When routing, drop all the cards except of the main ones (header, sidebar, meta).
+    # When routing, drop all the cards except of the main ones
+    # (header, sidebar, meta).
+    clear_cards(q)
 
-    
     add_card(q, 'article', ui.markdown_card(
         box='horizontal',
         title='',
-        content= '<div align="center"><h2>Overall topics</h2></div>',
-        
+        content='<div align="center"><h2>Overall topics</h2></div>',
     ))
     await q.page.save()
