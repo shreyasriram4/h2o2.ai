0e97a2f7783ce708b4bf1a78df50a339024b3d72
cliftonfelix <cliftonfelix12@yahoo.com>
2023-03-25 22:31:30 +0800

    Implement pycodestyle in sentiment analysis train.py

--- src/models/sentiment_analysis/train/train.py
+++ src/models/sentiment_analysis/train/train.py
@@ -2,12 +2,13 @@ from sklearn.model_selection import train_test_split
 from src.models.sentiment_analysis.train.bert import BERT
 from src.utils.file_util import FileUtil
 
+
 def main():
     df = FileUtil.get_processed_train_data()
-    train, valid = train_test_split(df, test_size = 0.2)
+    train, valid = train_test_split(df, test_size=0.2)
 
-    train = train.reset_index(drop = True)
-    valid = valid.reset_index(drop = True)
+    train = train.reset_index(drop=True)
+    valid = valid.reset_index(drop=True)
 
     model = BERT()
 
@@ -16,6 +17,7 @@ def main():
     ap, pr_auc = model.evaluate(valid)
 
     model.plot_training_acc_loss(history)
+
 
 if __name__ == "__main__":
     main()
