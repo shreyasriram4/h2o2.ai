80fde3b8ebf9e18bf094f8642dfbb8f86d82dd92
leeweiqing <71245649+leeweiqing@users.noreply.github.com>
2023-03-26 11:50:34 +0800

    Edited dashboard_viz functions

--- src/visualisation/dashboard_viz.py
+++ src/visualisation/dashboard_viz.py
@@ -56,75 +56,36 @@ async def topics_line_chart_over_time(data):
 
 async def topics_pie_chart(data):
     fig = px.pie(data, 'topic', title = "Frequency of Topics", color_discrete_sequence=px.colors.qualitative.Safe)
-    html = pio.to_html(fig, config=None, auto_play=True, include_plotlyjs="cdn")
-    return html
-
-async def visualise_all_topics(data):
-    return visualise_top_words(data, labels = CONFIG_PARAMS["labels"], specific = False, custom_sw = CONFIG_PARAMS["custom_stopwords"])
-
-async def visualise_all_topics(data, topic):
-    return visualise_top_words(data, labels = topic, specific = True, custom_sw = CONFIG_PARAMS["custom_stopwords"])
-    
-async def extract_top_reviews(data, topic):
-    topic_df = data[data["pred_topic_label"] == topic]
-    topic_sliced = list(topic_df.sort_values(by = "score").head(5)["partially_cleaned_text"])
-    return topic_sliced
-
-async def topics_bar_chart_by_month(data):
-    data['year_month'] = data['date'].dt.to_period('M').astype('string')
-    print(data)
-    freq_df = data.groupby(['year_month', 'topic'], as_index = False).size()
-    fig = px.area(freq_df, x="year_month", y="size", color = "topic", title="Topics over Time", color_discrete_sequence=px.colors.qualitative.Safe) 
     update_chart(fig)
     html = pio.to_html(fig, config=None, auto_play=True, include_plotlyjs="cdn")
     return html
 
-async def extract_num_topics(data):
-    count_df = data.groupby(['topic'], as_index = False).size()
-    return "in progress"
+async def visualise_all_topics(data):
+    fig = visualise_top_words(data, labels = CONFIG_PARAMS["labels"], specific = False, custom_sw = CONFIG_PARAMS["custom_stopwords"])
+    update_chart(fig)
+    html = pio.to_html(fig, config=None, auto_play=True, include_plotlyjs="cdn")
+    return html
 
+async def visualise_all_topics(data, topic):
+    fig = visualise_top_words(data, labels = topic, specific = True, custom_sw = CONFIG_PARAMS["custom_stopwords"])
+    update_chart(fig)
+    html = pio.to_html(fig, config=None, auto_play=True, include_plotlyjs="cdn")
+    return html
+    
+async def extract_top_reviews(data, topic):
+    topic_df = data[data["topic"] == topic]
+    topic_sliced = list(topic_df.sort_values(by = "score").head(5)["partially_cleaned_text"])
+    return topic_sliced
 
-
-
-# def reformat_data(data):
-#     data = data.assign(sentiment = data.sentiment.map({1: "positive", 0: "negative"}))
-#     data["date"] = pd.to_datetime(data["date"])
-#     return data
-
-# async def sentiment_pie_chart(data):
-#     fig = px.pie(data, names='sentiment', title='Overall Sentiments', color_discrete_sequence=px.colors.qualitative.Safe[0:2])
+# async def topics_bar_chart_by_month(data):
+#     data['year_month'] = data['date'].dt.to_period('M').astype('string')
+#     print(data)
+#     freq_df = data.groupby(['year_month', 'topic'], as_index = False).size()
+#     fig = px.area(freq_df, x="year_month", y="size", color = "topic", title="Topics over Time", color_discrete_sequence=px.colors.qualitative.Safe) 
 #     update_chart(fig)
 #     html = pio.to_html(fig, config=None, auto_play=True, include_plotlyjs="cdn")
 #     return html
 
-# def sentiment_line_chart_over_time(data):
-#     freq_df = data.groupby(['date', 'sentiment'], as_index = False).size()
-#     fig = px.line(freq_df, x="date", y="size", color = "sentiment", title="Sentiments over Time", color_discrete_sequence=px.colors.qualitative.Safe[0:2][::-1]) 
-#     return fig
-
-# def topics_bar_chart(data):
-#     freq_df = data.groupby(['pred_topic_label', 'sentiment'], as_index = False).size()
-#     freq_df['pct'] = freq_df.groupby('pred_topic_label', group_keys = False)['size'].apply(lambda x: np.round(x*100/x.sum(), 1))
-#     fig = px.bar(freq_df, x='pred_topic_label', y='pct', color = 'sentiment', color_discrete_sequence=px.colors.qualitative.Safe[0:2][::-1])
-#     fig.update_layout(xaxis={"dtick":1})
-
-#     return fig
-
-# def topics_line_chart_over_time(data):
-#     freq_df = data.groupby(['date', 'pred_topic_label'], as_index = False).size()
-#     fig = px.area(freq_df, x="date", y="size", color = "pred_topic_label", title="Topics over Time", color_discrete_sequence=px.colors.qualitative.Safe) 
-#     return fig
-
-# def topics_pie_chart(data):
-#     fig = px.pie(data, 'pred_topic_label', title = "Frequency of Topics", color_discrete_sequence=px.colors.qualitative.Safe)
-#     html = pio.to_html(fig, config=None, auto_play=True, include_plotlyjs="cdn")
-#     return fig
-
-# def visualise_all_topics(data):
-#     return visualise_top_words(data, labels = CONFIG_PARAMS["labels"], specific = False, custom_sw = CONFIG_PARAMS["custom_stopwords"])
-
-# def visualise_all_topics(data, topic):
-#     return visualise_top_words(data, labels = topic, specific = True, custom_sw = CONFIG_PARAMS["custom_stopwords"])
-    
-# def extract_top_reviews(data):
+# async def extract_num_topics(data):
+#     count_df = data.groupby(['topic'], as_index = False).size()
 #     return "in progress"
