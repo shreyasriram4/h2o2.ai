5bc87c9b5618b8be9db7ddbeaac5dd970ce29c7d
cliftonfelix <cliftonfelix12@yahoo.com>
2023-04-02 23:24:04 +0800

    Add docstring in zero_shot.py

--- src/models/topic_modelling/test/zero_shot.py
+++ src/models/topic_modelling/test/zero_shot.py
@@ -1,3 +1,5 @@
+"""This module contains ZeroShot topic model class."""
+
 import os
 import pandas as pd
 from transformers import pipeline
@@ -7,13 +9,17 @@ from src.utils.file_util import FileUtil
 
 
 class ZeroShot(Classifier):
+    """ZeroShot topic model class."""
+
     def save_model(self):
+        """Save ZeroShot model from storage."""
         classifier = pipeline(task="zero-shot-classification",
                               model="facebook/bart-large-mnli",
                               device=-1)
         FileUtil.put_topic_model(classifier)
 
     def get_model(self):
+        """Get ZeroShot model from storage."""
         model_file_path = os.path.join(FileUtil().TOPIC_MODELLING_DIR,
                                        FileUtil().MODEL_FILE_NAME)
         if not FileUtil.check_filepath_exists(model_file_path):
@@ -21,10 +27,31 @@ class ZeroShot(Classifier):
         return FileUtil.get_topic_model()
 
     def dataloader(self, df, column):
+        """
+        Generate each row text from df.
+
+        Args:
+          df (pd.DataFrame): dataframe to retrieve text
+          column (str): text column in df
+
+        Yields:
+          text value in column
+        """
         for i in range(len(df)):
             yield df.loc[i, column]
 
     def predict(self, df, column, candidate_labels):
+        """
+        Predict the topic for df.
+
+        Args:
+          df (pd.DataFrame): dataframe to predict
+          column (str): text column in df
+          candidate_labels (list): list of topics
+
+        Returns:
+          dataframe (pd.Dataframe): prediction result dataframe
+        """
         clf = self.get_model()
         hypothesis_template = "The topic of this review is {}."
 
@@ -40,9 +67,11 @@ class ZeroShot(Classifier):
         return df
 
     def fit(self):
+        """Fit ZeroShot topic model"""
         pass
 
     def evaluate(self):
+        """Evaluate topic classification."""
         pass
 
 
