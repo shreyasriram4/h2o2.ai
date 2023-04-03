14683fd35d4a4e99ea1ccd3db823d55939362ffa
hadragon26 <87232777+hadragon26@users.noreply.github.com>
2023-04-04 00:32:36 +0800

    merge conflict

--- src/models/sentiment_analysis/train/train.py
+++ src/models/sentiment_analysis/train/train.py
@@ -1,7 +1,7 @@
 """This module contains sentiment_analysis_train function."""
 
 from sklearn.model_selection import train_test_split
-# from src.models.sentiment_analysis.train.bert import BERT
+from src.models.sentiment_analysis.train.bert import BERT
 from src.models.sentiment_analysis.train.logreg import LOGREG
 from src.utils.file_util import FileUtil
 import joblib
@@ -22,13 +22,13 @@ def sentiment_analysis_train():
     train = train.reset_index(drop=True)
     valid = valid.reset_index(drop=True)
 
-    # bert_model = BERT()
-    # trained_bert_model, history = bert_model.fit(train.copy(), valid.copy())
-    # trained_bert_model.save_pretrained(FileUtil().BERT_SENTIMENT_MODEL_DIR)
-    # bert_ap, bert_pr_auc = bert_model.evaluate(valid.copy())
-    # bert_model.plot_training_acc_loss(history)
+    bert_model = BERT()
+    trained_bert_model, history = bert_model.fit(train.copy(), valid.copy())
+    trained_bert_model.save_pretrained(FileUtil().BERT_SENTIMENT_MODEL_DIR)
+    bert_ap, bert_pr_auc = bert_model.evaluate(valid.copy())
+    bert_model.plot_training_acc_loss(history)
 
-    #  LogReg and LSTM training then add to the metrics below
+     LogReg and LSTM training then add to the metrics below
 
     logreg_model = LOGREG()
     df_logreg = df.copy()
@@ -44,8 +44,8 @@ def sentiment_analysis_train():
     logreg_ap, logreg_pr_auc = logreg_model.evaluate(valid)
 
     FileUtil.put_metrics("sentiment_analysis",
-                         #  {"BERT": {"PR AUC": bert_pr_auc,
-                         #            "Average Precision": bert_ap}},
+                          {"BERT": {"PR AUC": bert_pr_auc,
+                                    "Average Precision": bert_ap}},
                          {"LOGREG": {"PR AUC": logreg_pr_auc,
                                      "Average Precision": logreg_ap}})
 
